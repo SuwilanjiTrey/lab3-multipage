7bdd957e386213a511cfbbc4052fde9acda76d6e
Suwilanji Trey Chellah <143183924+SuwilanjiTrey@users.noreply.github.com>
2025-04-08 00:19:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ document.getElementById("themeToggle").addEventListener("click", () =>
 {
 document.body.classList.toggle("dark-theme");
 });
+
+document.getElementById("contactForm").addEventListener("submit", (e) 
+=> {
+  e.preventDefault();
+  const name = document.getElementById("nameInput").value.trim();
+  const message = document.getElementById("messageInput").value.trim();
+
+
+  if (name === "" || message === ""){
+    alert("please fill out all field");
+  } else {
+    document.getElementById("Response").innerText = `Thanks, ${name}. we'll get back to you soon!`;
+
+    e.target.reset();
+    } 
+  }
+})
